1e7a976fc3823a748b847be7926ee8c0fed96cac
HaraRie-74 <121597855+HaraRie-74@users.noreply.github.com>
2023-03-02 06:04:17 +0000

    [add]コメント非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
-    book=Book.find(params[:book_id])
-    comment=BookComment.new(book_comment_params)
-    comment.book_id=book.id
-    comment.user_id=current_user.id
-    comment.save
-    redirect_back(fallback_location:root_path)
+    @book=Book.find(params[:book_id])
+    @comment=BookComment.new(book_comment_params)
+    @comment.book_id=@book.id
+    @comment.user_id=current_user.id
+    @comment.save
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location:root_path)
+    @book=Book.find(params[:book_id])
+    @comment=BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
